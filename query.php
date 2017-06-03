57256515c6a2592bd0bfa547c75e0120a2c062d7
Vorcane <vorcane@gmail.com>
2017-06-03 18:46:01 +0000

    Rewrote server code from scratch, should be able to handle any situation it encounters.

--- query.php
+++ query.php
@@ -1,11 +1,21 @@
-<?php
-	require('sslLabsApi.php'); //Calls the SSL labs API file
-	$api = new sslLabsApi(); //Creates a new API object
+<?php 
+	require('sslLabsApi.php'); //Calls the SSL labs API file 
+	$repeat = TRUE; //Defaults to the loop repeating
+	$api = new sslLabsApi(); //Creates a new API object 
+	$api->setReturnJsonObjects(TRUE); //Forces the API to return an object for easier processing
 	$sdomain = trim($_POST["domain"]); //Grabs the domain from the post request
-	var_dump($api->fetchHostInformation($sdomain)); //Dumps the variables from the object to console
-	$myvar = ob_get_clean(); //Grabs the console output and saves it. This is pretty dirty but it will do until I can think of something better
-	$gradeindex = strpos($myvar, "gradeTrustIgnored"); //Finds the index of the field we are looking for
-	$gradeindex = $gradeindex + 21; //Adds the set amount of spaces to the index to extract only the grade
-	$grade = substr($myvar, $gradeindex, 1); //Substrings from the console output to just grab the grade
-	echo "$grade"; //Returns the grade
+	$statusObject = $api->fetchHostInformation($sdomain); //Retrieves the initial host object to check status
+	do {
+		$status = $statusObject->status; //Checks if the request is finished or is still processing qualy side
+		if ($status != "READY") { //If process is not ready sleeps then tries again
+			sleep(30); //Arbitrary value, seems about right
+			$statusObject = $api->fetchHostInformation($sdomain); //Resends the request to the API to check if ready yet
+		} else {
+			$repeat = FALSE; //Sets repeat to false so the loop can exit
+               		$endpointObject = $statusObject->endpoints; //Retrieves the endpoints from the host object to extract grade
+                	$gradeObject = $endpointObject[0]; //The endpoint object is actually an array of every server on the website, this defaults to the first one
+        	        $grade = $gradeObject->gradeTrustIgnored; //Extracts the grade ignoring trust from the endpoint object
+			echo "$grade"; //Sends the grade back to the client
+		}
+	} while ($repeat); //If test isnt complete loops back
 ?>
